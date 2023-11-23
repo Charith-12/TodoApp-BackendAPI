41cdcd8be981b1c47b3078ae0ca3e48bc4afc0e1
Charith-12 <charithj12@gmail.com>
2023-11-24 00:40:35 +0530

    Add project files.

---
+++ TodoApp-BackendAPI/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp_BackendAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
